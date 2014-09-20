059ee6a2634dde022abf81c9a0561f49a39c684d
susheel <susheel.suresh@gmail.com>
2014-09-20 12:14:59 +0530

    First Commit

---
+++ mlclass-ex2-006/mlclass-ex2/main.m
@@ -0,0 +1,97 @@
+%% Machine Learning Online Class - Exercise 2: Logistic Regression
+%
+%  Instructions
+%  ------------
+% 
+%  This file contains code that helps you get started on the logistic
+%  regression exercise. You will need to complete the following functions 
+%  in this exericse:
+%
+%     sigmoid.m
+%     costFunction.m
+%     predict.m
+%     costFunctionReg.m
+%
+%  For this exercise, you will not need to change any code in this file,
+%  or any other files other than those mentioned above.
+%
+
+%% Initialization
+clear ; close all; clc
+
+%% Load Data
+%  The first two columns contains the exam scores and the third column
+%  contains the label.
+
+data = load('train.txt');
+X = data(:, [2,3,4,5,6]); y = data(:, 1);
+
+
+%% ============ Part 2: Compute Cost and Gradient ============
+%  In this part of the exercise, you will implement the cost and gradient
+%  for logistic regression. You neeed to complete the code in 
+%  costFunction.m
+
+%  Setup the data matrix appropriately, and add ones for the intercept term
+[m, n] = size(X);
+
+% Add intercept term to x and X_test
+X = [ones(m, 1) X];
+
+% Initialize fitting parameters
+initial_theta = zeros(n + 1, 1);
+
+% Compute and display initial cost and gradient
+[cost, grad] = costFunction(initial_theta, X, y);
+
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+fprintf('Gradient at initial theta (zeros): \n');
+fprintf(' %f \n', grad);
+
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
+
+
+%% ============= Part 3: Optimizing using fminunc  =============
+%  In this exercise, you will use a built-in function (fminunc) to find the
+%  optimal parameters theta.
+
+%  Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 1000);
+
+%  Run fminunc to obtain the optimal theta
+%  This function will return theta and the cost 
+[theta, cost] = ...
+	fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+
+% Print theta to screen
+fprintf('Cost at theta found by fminunc: %f\n', cost);
+fprintf('theta: \n');
+fprintf(' %f \n', theta);
+
+
+
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
+data1 = load('test.txt');
+X = data1(:,:);
+[m, n] = size(X);
+
+X = [ones(m, 1) X];
+p = zeros(m,1);
+
+
+
+for i = 1:m
+	if (sigmoid(theta' * (X(i,:))') >= 0.5)
+		p(i,:) = 1;
+	else
+		p(i,:) = 0;
+	endif
+
+end
+size(p)
+fprintf('\nProgram paused. Press enter to continue.\n');
+save result1.txt p -ascii
+pause;
+
